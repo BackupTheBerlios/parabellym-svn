c6ca8cb677be7dd9bc2e348c2cb3cd9baf55e70d
vhex <vhex@032a57a9-c0ef-0310-92a3-f2dbbc20f6fa>
2005-05-01 10:16:54 +0000

    Caching os.path.exist() calls.

--- branches/scons/faerion.py
+++ branches/scons/faerion.py
@@ -8,66 +8,86 @@ Faerion macros for SCons.
 # Copyright (C) 2005 The Faerion Team
 #
 # $Id$
-# vim:ts=2:sts=2:sw=2:et:nocindent:syntax=python
 #
 
 import os
 import re
 import shutil
+import string
 import SCons.Defaults
 
 class Faerion:
-  # A function to list files in a directory.
-  def listFilesW(self, at, r):
-    nodes = []
-    for location in at:
-      for file in os.listdir(location):
-        fname = os.path.join(location, file)
-        if os.path.isfile(fname):
-          if r.match(file):
-            nodes.append(fname)
-        elif os.path.isdir(fname):
-          if not os.path.basename(fname) == '.svn':
-            for more in self.listFilesW([fname], r):
-              nodes.append(more)
-    return nodes
+	def listFilesW(self, at, r):
+		nodes = []
+		for location in at:
+			for file in os.listdir(location):
+				fname = os.path.join(location, file)
+				if os.path.isfile(fname):
+					if r.match(file):
+						nodes.append(fname)
+				elif os.path.isdir(fname):
+					if not os.path.basename(fname) == '.svn':
+						for more in self.listFilesW([fname], r):
+							nodes.append(more)
+		return nodes
 
-  def listFiles(self, at, r):
-    l = self.listFilesW(at, re.compile(r))
-    l.sort()
-    return l
+	def listFiles(self, at, r):
+		l = self.listFilesW(at, re.compile(r))
+		l.sort()
+		return l
 
-  def compile(self, env, paths):
-    objs = []
-    for item in self.listFiles(paths, r'^.*(\.cc)$'):
-      fname, ext = os.path.splitext(item)
-      if env.has_key('category'):
-        fname = fname + '~' + env['category']
-      if env['debug'] == 1:
-        fname = fname + '~debug'
-      objs.append(env.SharedObject(fname + '.o', item))
-    return objs
+	def compile(self, env, paths):
+		objs = []
+		for item in self.listFiles(paths, r'^.*(\.cc)$'):
+			fname, ext = os.path.splitext(item)
+			if env.has_key('category'):
+				fname = fname + '~' + env['category']
+			if env['debug'] == 1:
+				fname = fname + '~debug'
+			objs.append(env.SharedObject(fname + '.o', item))
+		return objs
 
-  def makeInstaller(self, xenv, name, fmask):
-    env = xenv.Copy(TARFLAGS = '-c -z')
+	def makeInstaller(self, xenv, root, name, fmask):
+		dir = name
+		tar = name + '-src.tar.gz'
 
-    dir = name
-    tar = name + '-src.tar.gz'
+		if xenv.GetOption('clean'):
+			if os.path.exists(tar):
+				os.unlink(tar)
+		else:
+			env = xenv.Copy(TARFLAGS = '-c -z')
+			# the list of created directories, to avoid
+			# excessive filesystem calls.
+			edirs = []
 
-    for item in self.listFiles(['.'], fmask):
-      f = env.File(item)
+			if os.path.exists(dir):
+				shutil.rmtree(dir)
 
-      src = f.path
-      dst = dir + '/' + os.path.dirname(f.path)
-      dsf = dir + '/' + f.path
+			for item in self.listFiles([root], fmask):
+				f = env.File(item)
 
-      if not os.path.exists(dst):
-        os.makedirs(dst)
+				src = f.path
+				dst = dir + '/' + os.path.dirname(f.path)
+				dsf = dir + '/' + f.path
 
-      shutil.copy2(src, dsf)
-      env.Tar(tar, dsf)
+				if not dst in edirs:
+					if not os.path.exists(dst):
+						os.makedirs(dst)
+						edirs = edirs + [dst]
 
-    env.AddPostAction(tar, SCons.Defaults.Delete(dir))
+				shutil.copy2(src, dsf)
+				env.Tar(tar, dsf)
+
+			env.AddPostAction(tar, SCons.Defaults.Delete(dir))
+
+	# Applies sereval known fixes to the environment before configuring.
+	def PreConf(self, env):
+		if os.environ.has_key('CPATH'):
+			for em in string.split(os.environ['CPATH'], ';'):
+				env.Append(CPPPATH = em)
+				print 'Found directory %s in the environment.' % (em)
 
 def Build():
 	return Faerion()
+
+# vim:ts=4:sts=4:sw=4:noet:nocindent:syntax=python
